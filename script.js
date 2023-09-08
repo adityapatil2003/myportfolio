0dce50c96c7a6a7b501419be0b204f8a2787ae9e
aditya-patil <adityaonkarpatil@gmail.com>
2023-09-08 23:20:10 +0530

    some changes done

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
 
+
 // ------Sticky Navbar-------
 
-window.addEventListener('scroll', function(){
-    let navbar = document.getElementById('navbar')
-    if(window.scrollY > 30){
-        navbar.classList.add("sticky-nav")
-    }
-    else{
-        navbar.classList.remove("sticky-nav")
-
-    }
-})
+window.addEventListener("scroll", function () {
+  let navbar = document.getElementById("navbar");
+  if (window.scrollY > 30) {
+    navbar.classList.add("sticky-nav");
+  } else {
+    navbar.classList.remove("sticky-nav");
+  }
+});
 
 // ------Hamburger Menu-------
 
 var navLinks = document.getElementById("navLinks");
 
-      function showMenu() {
-        navLinks.style.top = "170px";
-      }
-      function hideMenu() {
-        navLinks.style.top = "-200px";
-      }
+function showMenu() {
+  navLinks.style.top = "170px";
+}
+function hideMenu() {
+  navLinks.style.top = "-200px";
+}
 
 // ----------footer-date----------
 
-let footerDate = document.getElementById("footer-date")
+let footerDate = document.getElementById("footer-date");
 footerDate.innerHTML = new Date().getFullYear();
 
 // ----------Got to top arrow----------
 
-let go_up = document.getElementById('top')
+let go_up = document.getElementById("top");
 
-window.addEventListener('scroll', function(){
-  if(window.scrollY > 500){
-    go_up.style.left="94%"
-  }else{
-    go_up.style.left="120%"
-
+window.addEventListener("scroll", function () {
+  let mq = window.matchMedia("(max-width: 700px)");
+  // let match = document.querySelector("#top");
+  if (window.scrollY > 500) {
+    if (mq.matches) {
+      go_up.style.left = "90%";
+    } else {
+      go_up.style.left = "94%";
+    }
+  } else {
+    go_up.style.left = "120%";
   }
-})
+});
 
 // ----------Theme switch----------
 
-let sun = document.getElementById("sun")
+let sun = document.getElementById("sun");
 
-sun.onclick = function(){
-  document.body.classList.toggle("light-theme")
-  if(document.body.classList.contains("light-theme")){
+sun.onclick = function () {
+  document.body.classList.toggle("light-theme");
+  if (document.body.classList.contains("light-theme")) {
     sun.className = "fa-regular fa-moon";
-  }else{
+  } else {
     sun.className = "fa-regular fa-sun";
-
   }
-
-}
+};
